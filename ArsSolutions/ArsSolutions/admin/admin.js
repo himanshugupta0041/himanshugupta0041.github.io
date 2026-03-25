7ad6db4231501e28be54495e95386de12685c0c7
himanshugupta0041 <hg5079424@gmail.com>
2026-03-25 15:24:24 +0530

    Update admin.js

--- ArsSolutions/ArsSolutions/admin/admin.js
+++ ArsSolutions/ArsSolutions/admin/admin.js
@@ -10,7 +10,7 @@ fetch("../data/products.json")
 
 function $(id) { return document.getElementById(id); }
 
-// Render all products with Edit/Delete buttons
+
 function renderProductList() {
     const box = $("productList");
     box.innerHTML = "";

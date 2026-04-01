24d6912a925403fa0fb728122fe50e412ec0aa9f
himanshugupta0041 <hg5079424@gmail.com>
2026-04-01 21:00:52 +0530

    Update laptop_categories.js

--- ArsSolutions/ArsSolutions/components/js/laptop_categories.js
+++ ArsSolutions/ArsSolutions/components/js/laptop_categories.js
@@ -46,7 +46,7 @@ function renderLaptopProducts(list) {
                 alt="${p.name}"
                 class="w-full h-full object-contain bg-white group-hover:scale-105 transition-transform duration-500">
 
-            <!-- BADGE: Discount -->
+         
             ${p.discount ? `
               <span class="absolute top-4 left-4 bg-primary text-white text-xs font-bold px-3 py-1 rounded-full uppercase tracking-wide">
                 ${p.discount}
